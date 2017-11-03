59a34afcf12f65aa2438423adfbf9e35318efd37
Brian Patrick <batpat772@gmail.com>
2017-11-03 10:41:35 -0700

    more stuff

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 var friends = require('./app/data/friends.js');
 
-console.log(friends.profiles);
+console.log(friends);
 //Sets up an instance for Express app
 //==========================================================
 var app = express();
@@ -25,50 +25,50 @@ app.use(bodyParser.json({type: "application/vnd.api+json"}))
 
 //Profiles to compare results to
 //==========================================================
-var profiles = [
-   {
-      name: "StarLord",
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/5/59/Star-Lord_GOTG_Vol_2.jpg/revision/latest/scale-to-width-down/1000?cb=20170505225132",
-      scores: [
+// var profiles = [
+//    {
+//       name: "StarLord",
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/5/59/Star-Lord_GOTG_Vol_2.jpg/revision/latest/scale-to-width-down/1000?cb=20170505225132",
+//       scores: [
 
-      ]
-   },
-   {
-      name: "Gamora",
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/5/53/Gamora_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170505225432",
-      scores: [
+//       ]
+//    },
+//    {
+//       name: "Gamora",
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/5/53/Gamora_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170505225432",
+//       scores: [
 
-      ]
-   },
-   {
-      name: "Rocket",
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/2/27/Rocket_Raccoon_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170505225943",
-      scores: [
+//       ]
+//    },
+//    {
+//       name: "Rocket",
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/2/27/Rocket_Raccoon_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170505225943",
+//       scores: [
 
-      ]
-   },
-   {
-      name: 'Yondu',
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/1/16/Yondu_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170503005847",
-      scores: [
+//       ]
+//    },
+//    {
+//       name: 'Yondu',
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/1/16/Yondu_GOTG_Vol_2.png/revision/latest/scale-to-width-down/1000?cb=20170503005847",
+//       scores: [
 
-      ]
-   },
-   {
-      name: "Drax",
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/7/7c/Drax_GOTG_Vol_2.jpg/revision/latest/scale-to-width-down/1000?cb=20170505225749",
-      scores: [
+//       ]
+//    },
+//    {
+//       name: "Drax",
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/7/7c/Drax_GOTG_Vol_2.jpg/revision/latest/scale-to-width-down/1000?cb=20170505225749",
+//       scores: [
 
-      ]
-   },
-   {
-      name: "Ego",
-      photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/a/aa/Ego_GOTG_Vol_2.png/revision/latest?cb=20170505230509",
-      scores: [
+//       ]
+//    },
+//    {
+//       name: "Ego",
+//       photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/a/aa/Ego_GOTG_Vol_2.png/revision/latest?cb=20170505230509",
+//       scores: [
          
-      ]
-   }
-];
+//       ]
+//    }
+// ];
 
 //Routes
 //==========================================================
